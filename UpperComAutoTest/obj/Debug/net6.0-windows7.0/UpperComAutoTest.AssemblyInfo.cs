fa57a2506e77370c14dea726747ee1bc6b8ca4af
世沛 王 <1016423262@qq.com>
2024-09-09 21:33:38 +0800

    xx

--- UpperComAutoTest/obj/Debug/net6.0-windows7.0/UpperComAutoTest.AssemblyInfo.cs
+++ UpperComAutoTest/obj/Debug/net6.0-windows7.0/UpperComAutoTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpperComAutoTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+365721c672120d4a730f60cfd640ee7727df3551")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e2c1174b49ea8097f6782611466eed913eefad")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpperComAutoTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpperComAutoTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
